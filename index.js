6643aabf98b2feb9b93fb86a5e65152635bb8117
ShuvenduSamanta <shuvendu247@gmail.com>
2023-01-03 23:31:14 +0530

    user data model

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import path from "path";
 import morgan from "morgan";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 /* CONFIGURATION */
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +34,9 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer ({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
